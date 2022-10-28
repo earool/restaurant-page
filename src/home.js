c90f3a4858b954032e322b13f3910a02b823d6ec
earool <dawid.baranski0000@gmail.com>
2022-10-28 12:58:47 +0200

    Add content in main

--- src/home.js
+++ src/home.js
@@ -3,14 +3,14 @@ export default function loadHomePage() {
     content.classList.add('content');
 
     // header
-    const header = document.createElement('div');
-    header.classList.add('header');
-    header.classList.add('round-corners');
+    const header = document.createElement('header');
+    header.classList.add('header', 'container',
+     'round-corners', 'container-size');
     content.appendChild(header);
     
-    const title = document.createElement('h1');
-    title.innerText = 'Sweet-Longitudinal-Cylinders';
-    header.appendChild(title);
+    const titleHeader = document.createElement('h1');
+    titleHeader.innerText = 'Sweet-Longitudinal-Cylinders';
+    header.appendChild(titleHeader);
 
     const tabContainer = document.createElement('div');
     tabContainer.classList.add('tab-container');
@@ -23,6 +23,24 @@ export default function loadHomePage() {
         tab.innerText = tabs[i];
         tabContainer.appendChild(tab);
     }
+
+    // main
+
+    const main = document.createElement('main');
+    main.classList.add('container-size');
+    content.appendChild(main);
+    
+    const aboutMe = document.createElement('div');
+    aboutMe.classList.add('container', 'round-corners')
+    main.appendChild(aboutMe);
+    
+    const titleAbout = document.createElement('h3');
+    titleAbout.innerText = 'Lorem Ipsum' ;
+    aboutMe.appendChild(titleAbout);
+    
+    const textAbout = document.createElement('div');
+    textAbout.innerText = 'Ut hendrerit, risus vitae porttitor vestibulum, purus justo tempus massa, ac iaculis sem nibh sollicitudin felis. Sed fermentum aliquam pretium. Pellentesque consectetur gravida orci et vulputate. Donec eros velit, feugiat sed nisl et, pulvinar consectetur mi. Ut sit amet nulla tristique, porta dolor quis, viverra magna. Maecenas tincidunt, tortor in.'
+    aboutMe.appendChild(textAbout);
 
     
 }
